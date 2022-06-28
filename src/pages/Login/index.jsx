b61b3b28679365926a5b470cf3dc7db5bc9b2bf7
marcelostial <marceloaface@outlook.com>
2022-06-28 17:27:24 -0300

    feat: adicionando logo e html da pagina de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import './index.css'
-
+import Logo from '../../assents/logoInternSingUp.png'
 const Home = () => {
   return (
     <div className="main">
       <header>
+      <img draggable="false" src={Logo} alt="logo"></img>
         <h1>Intern Sing Up</h1>
       </header>
       <form>
-        <input>
-        </input>
       </form>
-      <button>Abrir
-      </button>
     </div>
   );
 };
