7ad725a3219bb12d9019e317ad1efa4a3d0253e5
marcelostial <marceloaface@outlook.com>
2022-06-28 16:59:43 -0300

    build: nova estrutura

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './index.css'
+
+const Home = () => {
+  return (
+    <div className="main">
+      <header>
+        <h1>Intern Sing Up</h1>
+      </header>
+      <form>
+        <input>
+        </input>
+      </form>
+      <button>Abrir
+      </button>
+    </div>
+  );
+};
+
+export default Home;
