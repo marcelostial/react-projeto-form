d119c0d106ab3f7edd80ee6e0a64a15d8e1585b6
Marcelo Araujo Silva <marcelo.s@compasso.com.br>
2022-06-28 13:24:56 -0300

    first commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css'
+
+const Home = () => {
+  return (
+    <div className="baseContainer">
+      <div id="container">
+      </div>
+    </div>
+  );
+};
+
+export default Home;
